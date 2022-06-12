764a21384658dc62dc93ffea9fd5813923df5106
swarag7499 <62467813+swarag7499@users.noreply.github.com>
2022-06-13 01:10:00 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,108 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What position does Harry play on his Quidditch team?",
+    answer: "Keeper",
+    options: [
+      "Keeper",
+      "Seeker",
+      "Bludger",
+      "Chaser"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Where Did Hermione Share Her Very First Kiss With Ron?",
+    answer: "The Chamber of Secrets",
+    options: [
+      "Ministry of Magic",
+      "Gryffindor common room",
+      "The Chamber of Secrets",
+      "The room of requirements"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When was the 1st time Harry and Jinny met?",
+    answer: "King's Cross Station",
+    options: [
+     "The Burrow",
+      "Hogwards",
+      "King's Cross Station",
+      "Diagon Alley"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How would you spell the spell you would use to disarm someone?",
+    answer: "Expelliarmus",
+    options: [
+     "Alohomora",
+      "Expelliarmus",
+      "Riddikulus",
+      "Lumos"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What was Harry’s Patronus?",
+    answer: "A stag",
+    options: [
+     "A deer",
+      "A stag",
+      "A rabbit",
+      "A horse"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Who Does Hermione Go To The Yule Ball With, Causing Extreme Jealously For Ron?",
+    answer: "Viktor Krum",
+    options: [
+     "Cormac McLaggen",
+      "Dean Thomas",
+      "Michal Corner",
+      "Viktor Krum"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What Spell Does Hermione Use On Cormac To Help Ron Win The Keeper Tryouts For Quidditch?",
+    answer: "confundus",
+    options: [
+     "confundus",
+      "Obliviate",
+      "Levicorpus",
+      "Arrest Momentum"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Swarag has Very Important question to ask 🥺, would you like to be his girl? ",
+    answer: "Definetly Yes",
+    options: [
+     "Definetly Yes",
+      "Yes",
+      "Maybe",
+      "Idk"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
