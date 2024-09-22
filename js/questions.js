8a09b9451c794882a1081bb688a421b594364cdc
Swarag Gutte <62467813+swarag7499@users.noreply.github.com>
2024-09-21 22:31:00 -0400

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -80,29 +80,5 @@ let questions = [
     ]
   },
 
-  {
-    numb: 8,
-    question: "Swarag has Very Important question to ask 🥺, would you like to be his girl? ",
-    answer: "Definetly Yes",
-    options: [
-     "Definetly Yes",
-      "Yes",
-      "Maybe",
-      "Idk"
-    ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+ 
 ];
